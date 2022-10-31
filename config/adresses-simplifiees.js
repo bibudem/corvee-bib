fb32d7eaf8e54be0a81a25cc18ad1a5bac1f120e
Christian Rémillard <christian.remillard@gmail.com>
2022-10-31 10:14:51 -0400

    Ajout de l'adresse simplifiée /sofia. Les adresses simplifiées contenant 'www.bib.umontreal.ca' sont maintenant attrapées.

--- config/adresses-simplifiees.js
+++ config/adresses-simplifiees.js
@@ -1,5 +1,5 @@
 export const adressesSimplifiees = [
-    /^https:\/\/bib\.umontreal\.ca\/(24-7|bci|calendrier|calypso|citer|code-conduite|deposer-theses|geoindex|livraison|maestro|outils-traducteur|papyrus|peb|periodiques|pret|proxy|revues|salles|bibliotheque-sciences|rediger-theses|sid|soutien-informatique|theses|unequestion|zones)$/i,
+    /^https:\/\/(www\.)?bib\.umontreal\.ca\/(24-7|bci|bibliotheque-sciences|calendrier|calypso|citer|code-conduite|deposer-theses|geoindex|livraison|maestro|outils-traducteur|papyrus|peb|periodiques|pret|proxy|rediger-theses|revues|salles|sid|sofia|soutien-informatique|theses|unequestion|zones)$/i,
     // Autres redirections sur bib.umontreal.ca
     /^https:\/\/bib\.umontreal\.ca\/(formation|gif|nouvelles-acquisitions|salle)$/i,
     /^https:\/\/bib\.umontreal\.ca\/(chercher\/mon-dossier|formations\/capsules-video-youtube)$/i
