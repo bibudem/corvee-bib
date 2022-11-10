c5d9491da281e074c4b69577e156874760f05506
Christian Rémillard <christian.remillard@gmail.com>
2022-11-10 15:23:05 -0500

    Ajout d'un bouton pour télécharger les images d'À La Carte.

--- filters/local/messages.js
+++ filters/local/messages.js
@@ -40,7 +40,7 @@ export default {
 
     "bib-lien-guides-bib-umontreal-ca": {
         pattern: "(.+)",
-        substitution: "Ce lien pointe vers <em>À La Carte</em> (<a href=\"$1\" class=\"cvw-url\">$1</a>). Or ce serveur va être prochainement fermé. Utilisez plutôt LibGuides pour héberger vos fichiers.",
+        substitution: "Ce lien pointe vers <em>À La Carte</em>. Or ce serveur va être prochainement fermé. Utilisez plutôt LibGuides pour héberger vos fichiers. <p class=\"text-center\"><a href=\"$1\" download class=\"btn btn-primary\"><span class=\"glyphicon glyphicon-download-alt\" aria-hidden=\"true\"></span> Télécharger ce fichier</a></p>",
         msg: "Ce lien pointe vers <em>À La Carte</em> (https://guides.bib.umontreal.ca/...). Or ce serveur va être prochainement fermé. Utilisez plutôt LibGuides pour héberger vos fichiers."
     },
 
