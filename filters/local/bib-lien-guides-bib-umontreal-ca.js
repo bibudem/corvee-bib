2641a9dd982711c40f1842a51c35f709b75525c1
Christian Rémillard <christian.remillard@gmail.com>
2022-12-14 17:12:22 -0500

    Correction au filtre bib-lien-guides-bib-umontreal-ca.

--- filters/local/bib-lien-guides-bib-umontreal-ca.js
+++ filters/local/bib-lien-guides-bib-umontreal-ca.js
@@ -2,7 +2,7 @@
 export default {
   code: 'bib-lien-guides-bib-umontreal-ca',
   test: (report) => {
-    if (report.url && report.url.startsWith('https://guides.bib.umontreal.ca')) {
+    if (report.url && report.url.startsWith('https://guides.bib.umontreal.ca') && report.httpStatusCode < 300) {
       report.finalUrl = null
       return report.url
     }
