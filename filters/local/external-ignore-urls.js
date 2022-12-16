65b9eec9d685553e833525d6ffbfcdfa22ecca9a
Christian Rémillard <christian.remillard@gmail.com>
2022-12-16 16:36:56 -0500

    Ajout de news.google.com à la liste des liens à ignorer

--- filters/local/external-ignore-urls.js
+++ filters/local/external-ignore-urls.js
@@ -2,11 +2,14 @@ export default {
     code: 'external-ignore-urls',
     description: 'Misc URLs to ignore from the Web',
     test: (report) => {
-        return ((
-            /^https?:\/\/dx\.doi\.org/i.test(report.url)
-            || /^https?:\/\/doi\.org\//i.test(report.url)
-            || /^https?:\/\/collections\.banq\.qc\.ca\/ark:/i.test(report.url) // permaliens Banq) 
-        ) && report.httpStatusCode < 400)
+        return (
+            (
+                /^https?:\/\/dx\.doi\.org/i.test(report.url)
+                || /^https?:\/\/doi\.org\//i.test(report.url)
+                || /^https?:\/\/collections\.banq\.qc\.ca\/ark:/i.test(report.url) // permaliens Banq) 
+            )
+            && report.httpStatusCode < 400
+        )
             || report.url === 'https://soquij.qc.ca'
             || report.url.startsWith('https://canliiconnects.org') && report.httpStatusCode === 404 // Faux 404. C'est en réalité un 200
             || (
@@ -17,6 +20,7 @@ export default {
                 report.url.startsWith('https://www.lawblogs.ca')
                 && report.httpStatusCode === 403 // Faux 403. C'est en réalité un 200
             )
+            || report.url.startsWith('https://news.google.com/')
     },
     exclude: true
 }
