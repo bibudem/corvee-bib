31796c743893b183b89bb3ced319ee42a1affd31
Christian Rémillard <christian.remillard@gmail.com>
2024-01-08 16:12:17 -0500

    feat(filter): Ajout de patrons d'URLs pour détecter des 404

--- config/urls-as-404.js
+++ config/urls-as-404.js
@@ -3,5 +3,7 @@ export const urlsAs404 = [
   /code=404($|#|\?)/i, // https://methods.sagepub.com/error/handleStatusCode?code=404
   /404\.(.+)/i, // http://www.cmq.org/erreurs/404.aspx?lang=fr&aspxerrorpath=%2fhub%2ffr%2fetudes-medicales-demandes-permis.asp, https://www.gatineau.ca/portail/default.aspx?p=404&r=&u=http%3a%2f%2fwww.gatineau.ca%2f404.html%3faspxerrorpath%3d%2fdonneesouvertes%2fdefault_fr.aspx
   /\/404($|\?|#)/, // https://soquij.qc.ca/a/fr/404, https://agriculture.canada.ca/fr/system/404?destination=/en&_exception_statuscode=404
-  'page_404?', // https://www.csse.org/site/page_404?url=https://www.csse.org/index.html
+  'page_404?', // https://www.csse.org/site/page_404?url=https://www.csse.org/index.html,
+  'ServerError404', // https://noc.esdc.gc.ca/Error/ServerError404
+  '404-error', // https://www.leeds.ac.uk/404-error
 ]
