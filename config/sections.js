0adfb099c78a34d5d9ceac54df8e669428d46a95
Christian Rémillard <christian.remillard@umontreal.ca>
2025-11-06 10:51:27 -0500

    feat(sections): Mise à jour de la liste des sections du site.

--- config/sections.js
+++ config/sections.js
@@ -9,107 +9,37 @@ export default [
     ],
   },
   {
-    title: 'Portails disciplinaires',
+    title: 'Site central',
     sections: [
       {
-        key: 'amenagement',
-        title: 'Aménagement',
-        urls: ['https://bib.umontreal.ca/amenagement'],
-      },
-      {
-        key: 'anthropologie-demographie-sociologie',
-        title: 'Anthropologie, démographie, sociologie',
-        urls: ['https://bib.umontreal.ca/anthropologie-demographie-sociologie'],
-      },
-      {
-        key: 'art-cinema-musique',
-        title: 'Art, cinéma, musique',
-        urls: ['https://bib.umontreal.ca/art-cinema-musique'],
-      },
-      {
-        key: 'communication-sciences-information',
-        title: "Communication, sciences de l'information",
-        urls: ['https://bib.umontreal.ca/communication-sciences-information'],
-      },
-      {
-        key: 'criminologie-psychologie-travail-social',
-        title: 'Criminologie, psychologie, travail social',
-        urls: ['https://bib.umontreal.ca/criminologie-psychologie-travail-social'],
-      },
-      {
-        key: 'droit',
-        title: 'Droit',
-        urls: ['https://bib.umontreal.ca/droit'],
-      },
-      {
-        key: 'economie-politique-relations-industrielles',
-        title: 'Économie, politique, relations industrielles',
-        urls: ['https://bib.umontreal.ca/economie-politique-relations-industrielles'],
-      },
-      {
-        key: 'education-psychoeducation',
-        title: 'Éducation, psychoéducation',
-        urls: ['https://bib.umontreal.ca/education-psychoeducation'],
-      },
-      {
-        key: 'etudes-religieuses-histoire-philosophie',
-        title: 'Études religieuses, histoire, philosophie',
-        urls: ['https://bib.umontreal.ca/etudes-religieuses-histoire-philosophie'],
-      },
-      {
-        key: 'informatique-mathematique-sciences-nature',
-        title: 'Informatique, mathématiques, sciences de la nature',
-        urls: ['https://bib.umontreal.ca/informatique-mathematique-sciences-nature'],
-      },
-      {
-        key: 'langues-litteratures',
-        title: 'Langues et littératures',
-        urls: ['https://bib.umontreal.ca/langues-litteratures'],
-      },
-      {
-        key: 'sciences-sante',
-        title: 'Sciences de la santé',
-        urls: ['https://bib.umontreal.ca/sciences-sante'],
-      },
-      {
-        key: 'multidisciplinaire',
-        title: 'Multidisciplinaire',
-        urls: ['https://bib.umontreal.ca/multidisciplinaire'],
-      },
-    ],
-  },
-  {
-    title: 'Autres sections du site',
-    sections: [
-      {
-        key: 'chercher',
-        title: "Chercher de l'information",
-          urls: ['https://bib.umontreal.ca/chercher', 'https://bib.umontreal.ca/guides', 'https://bib.umontreal.ca/emprunter', 'https://bib.umontreal.ca/obtenir'],
-      },
-      {
-        key: 'utiliser',
-        title: "Utiliser l'information",
-        urls: ['https://bib.umontreal.ca/citer', 'https://bib.umontreal.ca/evaluer-analyser-rediger', 'https://bib.umontreal.ca/gerer-diffuser', 'https://bib.umontreal.ca/formations'],
-      },
-      {
-        key: 'travailler',
-        title: 'Travailler en bibliothèque',
-        urls: ['https://bib.umontreal.ca/travailler'],
+        key: 'site-central',
+        title: 'Toutes les pages',
+        urls: ['https://bib.umontreal.ca/'],
       },
       {
         key: 'a-propos',
         title: 'À propos',
-        urls: ['https://bib.umontreal.ca/les-bibliotheques-udem', 'https://bib.umontreal.ca/faire-un-don', 'https://bib.umontreal.ca/communications', 'https://bib.umontreal.ca/services', 'https://bib.umontreal.ca/collections', 'https://bib.umontreal.ca/sondage-et-enquetes'],
+        urls: ['https://bib.umontreal.ca/a-propos'],
       },
       {
-        key: 'soutien-enseignement',
-        title: "Soutien à l'enseignement",
-        urls: ['https://bib.umontreal.ca/soutien-enseignement'],
+        key: 'engagements',
+        title: 'Engagements',
+        urls: ['https://bib.umontreal.ca/engagements'],
       },
       {
-        key: 'soutien-recherche',
-        title: 'Soutien à la recherche',
-        urls: ['https://bib.umontreal.ca/soutien-recherche', 'https://bib.umontreal.ca/recherche-documentaire-subventions-et-laboratoires'],
+        key: 'enseignement',
+        title: 'Enseignement',
+        urls: ['https://bib.umontreal.ca/enseignement'],
+      },
+      {
+        key: 'espaces',
+        title: 'Espaces',
+        urls: ['https://bib.umontreal.ca/espaces'],
+      },
+      {
+        key: 'etudes',
+        title: 'Études',
+        urls: ['https://bib.umontreal.ca/etudes'],
       },
       {
         key: 'nous-joindre',
@@ -117,15 +47,163 @@ export default [
         urls: ['https://bib.umontreal.ca/nous-joindre'],
       },
       {
+        key: 'nous-soutenir',
+        title: 'Nous soutenir',
+        urls: ['https://bib.umontreal.ca/nous-soutenir'],
+      },
+      {
+        key: 'obtenir',
+        title: 'Obtenir',
+        urls: ['https://bib.umontreal.ca/obtenir'],
+      },
+      {
+        key: 'recherche',
+        title: 'Recherche',
+        urls: ['https://bib.umontreal.ca/recherche'],
+      },
+      {
+        key: 'autres',
+        title: 'Autres',
+        urls: ['https://bib.umontreal.ca/accessibilite-web', 'https://bib.umontreal.ca/conditions-utilisation', 'https://bib.umontreal.ca/horaires', 'https://bib.umontreal.ca/service-accessibilite'],
+      },
+    ],
+  },
+  {
+    title: 'Boîte à outils',
+    sections: [
+      {
+        key: 'bo-toutes-les-pages',
+        title: 'Toutes les pages',
+        urls: ['https://boite-outils.bib.umontreal.ca/'],
+      },
+      {
+        key: 'amenagement',
+        title: 'Aménagement',
+        urls: ['https://boite-outils.bib.umontreal.ca/amenagement'],
+      },
+      {
+        key: 'anthropologie-demographie-sociologie',
+        title: 'Anthropologie, démographie, sociologie',
+        urls: ['https://boite-outils.bib.umontreal.ca/anthropologie-demographie-sociologie'],
+      },
+      {
+        key: 'art-cinema-musique',
+        title: 'Art, cinéma, musique',
+        urls: ['https://boite-outils.bib.umontreal.ca/art-cinema-musique'],
+      },
+      {
+        key: 'communication-sciences-information',
+        title: "Communication, sciences de l'information",
+        urls: ['https://boite-outils.bib.umontreal.ca/communication-sciences-information'],
+      },
+      {
+        key: 'criminologie-psychologie-travail-social',
+        title: 'Criminologie, psychologie, travail social',
+        urls: ['https://boite-outils.bib.umontreal.ca/criminologie-psychologie-travail-social'],
+      },
+      {
+        key: 'droit',
+        title: 'Droit',
+        urls: ['https://boite-outils.bib.umontreal.ca/droit'],
+      },
+      {
+        key: 'economie-politique-relations-industrielles',
+        title: 'Économie, politique, relations industrielles',
+        urls: ['https://boite-outils.bib.umontreal.ca/economie-politique-relations-industrielles'],
+      },
+      {
+        key: 'education-psychoeducation',
+        title: 'Éducation, psychoéducation',
+        urls: ['https://boite-outils.bib.umontreal.ca/education-psychoeducation'],
+      },
+      {
+        key: 'etudes-religieuses-histoire-philosophie',
+        title: 'Études religieuses, histoire, philosophie',
+        urls: ['https://boite-outils.bib.umontreal.ca/etudes-religieuses-histoire-philosophie'],
+      },
+      {
+        key: 'informatique-mathematique-sciences-nature',
+        title: 'Informatique, mathématiques, sciences de la nature',
+        urls: ['https://boite-outils.bib.umontreal.ca/informatique-mathematique-sciences-nature'],
+      },
+      {
+        key: 'langues-litteratures',
+        title: 'Langues et littératures',
+        urls: ['https://boite-outils.bib.umontreal.ca/langues-litteratures'],
+      },
+      {
+        key: 'sciences-sante',
+        title: 'Sciences de la santé',
+        urls: ['https://boite-outils.bib.umontreal.ca/sciences-sante'],
+      },
+      {
+        key: 'multidisciplinaire',
+        title: 'Multidisciplinaire',
+        urls: ['https://boite-outils.bib.umontreal.ca/multidisciplinaire'],
+      },
+
+      //
+
+      {
+        key: 'bo-recherche',
+        title: 'Recherche et cycles supérieurs',
+        urls: ['https://boite-outils.bib.umontreal.ca/recherche'],
+      },
+      {
+        key: 'bo-trouver-evaluer',
+        title: 'Trouver et évaluer de l’information',
+        urls: ['https://boite-outils.bib.umontreal.ca/trouver-evaluer'],
+      },
+      {
+        key: 'bo-citer',
+        title: 'Citations et bibliographies',
+        urls: ['https://boite-outils.bib.umontreal.ca/citer'],
+      }
+    ],
+  },
+  {
+    title: 'Studio.bib',
+    sections: [
+      {
+        key: 'st-toutes-les-pages',
+        title: 'Toutes les pages',
+        urls: ['https://studio.bib.umontreal.ca/'],
+      },
+      {
+        key: 'st-espaces',
+        title: 'Espaces',
+        urls: ['https://studio.bib.umontreal.ca/espaces'],
+      },
+      {
+        key: 'st-informatique',
+        title: 'Ressources informatique',
+        urls: ['https://studio.bib.umontreal.ca/informatique'],
+      },
+      {
+        key: 'st-creatives',
+        title: 'Technologies créatives',
+        urls: ['https://studio.bib.umontreal.ca/creatives'],
+      },
+      {
+        key: 'st-medias',
+        title: 'Productions médias',
+        urls: ['https://studio.bib.umontreal.ca/medias'],
+      },
+      {
+        key: 'st-a-propos',
+        title: 'À propos',
+        urls: ['https://studio.bib.umontreal.ca/a-propos'],
+      },
+    ],
+  },
+  {
+    title: 'Autres sections du site',
+    sections: [
+      {
         key: 'www',
-          title: 'Pages sur www.bib.umontreal.ca',
+        title: 'Pages sur www.bib.umontreal.ca',
         urls: ['https://www.bib.umontreal.ca'],
       },
-        {
-          key: 'autres',
-          title: 'Autres',
-          urls: ['https://bib.umontreal.ca/conditions-bibliotheques', 'https://bib.umontreal.ca/recherche-documentaire-subventions-et-laboratoires', 'https://bib.umontreal.ca/accessibilite'],
-        },
     ],
   },
 ]
