3a5eec1a38e5508fe531a9f542e3d914cf4f1d86
Christian Rémillard <christian.remillard@umontreal.ca>
2026-05-26 09:18:24 -0400

    feat: Exclusion des requêtes avec statut http 403

--- filters/local/http-403.js
+++ filters/local/http-403.js
@@ -1,8 +1,7 @@
-// http://opurl.bib.umontreal.ca:8331/V/?func=native-link&resource=MON04447
 export default {
   code: 'http-403',
   test: (report) => {
-    return report.errorCodes?.includes('http-403')
+    return report.httpStatusCode === 403 || report.errorCodes?.includes('http-403')
   },
   exclude: true
 }
